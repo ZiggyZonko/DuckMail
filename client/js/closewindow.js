67938ae688ff392d2d4554b199ec0d02f7ee2cb5
ZiggyZonko <zacharyalindley@gmail.com>
2026-03-10 16:40:56 +1100

    Updated Structure + Frontend

---
+++ client/js/closewindow.js
@@ -0,0 +1,4 @@
+function window_close() {
+    window.open('', '_self', ''); // Open a blank page in the current tab
+    window.close(); // Close the current tab
+}
--- server/express.js
+++ server/express.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 5503;
 
 app.use(express.static('../client'));
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // To support JSON-encoded bodies
 
 app.post("/submit", (req, res) => {
-    res.send("Form submitted!, Welcome to Duck Mail, " + req.body.text + " " + req.body.text1);
+    res.send("Form submitted!, Welcome to Duck Mail, " + req.body.email + " " + req.body.password);
 })
 
 app.listen(port, () => {
