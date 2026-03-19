87d43a5b31d1107b0adb9e3bc02f6f944e33390b
ZiggyZonko <zacharyalindley@gmail.com>
2026-03-19 20:28:50 +1100

    MongoDB Foundation Implementation

--- server/express.js
+++ server/express.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const bodyParser = require("body-parser");
+const path = require('path');
 
 const app = express();
 const port = 5503;
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/duckmail')
+  .then(() => console.log("Connected to MongoDB 🦆"))
+  .catch(err => console.log(err));
+
 app.use(express.static('../client'));
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json()); // To support JSON-encoded bodies
+
+app.set('view engine', 'ejs');
+app.set('views', '../client'); // Tell Express where your files are
 
 app.post("/submit", (req, res) => {
-    res.send("Form submitted!, Welcome to Duck Mail, " + req.body.email + " " + req.body.password);
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    console.log(`Received form submission: Email=${email}, Password=${password}`);
+    res.render('postmethod', { email: email, password: password });
+    //res.send("Form submitted!, Welcome to Duck Mail, " + req.body.email + " " + req.body.password);
 })
 
 app.listen(port, () => {
